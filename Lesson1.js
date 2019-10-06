00f4d6d95c879285b0be1ba6a1de5bd00e8f6118
unknown <gauvietvt@gmail.com>
2019-10-06 18:33:26 +0700

    update

---
+++ Lesson1.js
@@ -0,0 +1,14 @@
+//Say hello!
+console.log('hello world!');
+document.write('hello world!');
+
+//Variables
+let varibale = 2;
+//...and print it
+document.write("variable is"+varibale);
+
+//Operator
+console.log('1+1='+1+1); // +
+console.log('4/2='+4/2); // /
+console.log('5-4='+5-4); //-
+console.log('5*2='+5*2); //*
